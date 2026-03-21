3502072527893030e388de24cfb85f515d434e0a
Cássio Henrique <cassiohenriqueribeiroduraes@gmail.com>
2026-03-21 14:55:55 -0300

    primeira versao pwa

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'nexpro-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/autonomo.html',
+  '/gerente.html',
+  '/admin.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
